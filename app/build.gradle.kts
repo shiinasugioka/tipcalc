61d7aceb3cacc5166fcb7b2bd9c2a3aa3e3bc0c9
Shiina Sugioka <shiina12@uw.edu>
2023-10-26 15:55:33 -0700

    progress bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "edu.uw.ischool.shiina12.tipcalc"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "edu.uw.ischool.shiina12.tipcalc"
